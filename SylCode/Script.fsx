99351123fc65ef04ff5213def21a59df32ece4f7
Henrik Härkönen <henrik.harkonen@if.fi>
2016-06-09 08:49:58 +0300

    First real commit.

--- SylCode/Script.fsx
+++ SylCode/Script.fsx
@@ -1,8 +1,9 @@
 ﻿// Learn more about F# at http://fsharp.org. See the 'F# Tutorial' project
 // for more guidance on F# programming.
 
-#load "Library1.fs"
+#load "SylCode.fs"
 open SylCode
 
 // Define your library scripting code here
 
+SylCode.encode "609b0"
---
+++ SylCode/SylCode.fs
@@ -0,0 +1,42 @@
+﻿namespace SylCode
+
+open System.Collections
+open System
+
+module SylCode =
+
+    // By Gennady Loskutov from http://fssnip.net/gf
+    let fromHex (s:string) = 
+      s
+      |> Seq.windowed 2
+      |> Seq.mapi (fun i j -> (i,j))
+      |> Seq.filter (fun (i,j) -> i % 2=0)
+      |> Seq.map (fun (_,j) -> Byte.Parse(new System.String(j),System.Globalization.NumberStyles.AllowHexSpecifier))
+      |> Array.ofSeq
+
+
+    let letters = ['a'..'z']
+    let vowels = ['a'; 'e'; 'i'; 'o'; 'u'; 'y']
+
+    let double_ctos c = sprintf "%c%c" c c
+    let ctos c = sprintf "%c" c
+
+    let doublevowels = List.map double_ctos vowels
+    let vowelstrings = "ai" :: List.map ctos vowels // one extra vowel combination needed to exceed 256 different syllables
+
+    let all_vowels = List.append vowelstrings doublevowels
+
+    let consonants =
+        let consonant_set = (Set letters) - (Set vowels)
+        Set.toList consonant_set
+
+    let syllables = [ for x in consonants do
+                        for y in all_vowels do
+                            yield sprintf "%c%s" x y 
+                    ]
+
+    let byte_to_syl (input:byte) : string = syllables.Item (int input)
+
+    let encode (value:string) =
+        let ByteData = fromHex value
+        System.String.Concat(Array.map byte_to_syl ByteData)
